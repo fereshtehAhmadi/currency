95e988cb88c5ccb2f6126d6b46263ed44c0800b2
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-04-05 16:42:32 +0430

    use bs4

--- currency.py
+++ currency.py
@@ -1,15 +1,22 @@
+from bs4 import BeautifulSoup
 import requests
 
 
-url = 'https://api.exchangerate.host/latest'
+user = input("enter how much you have:  ")
+url = 'https://www.tgju.org/%D9%82%DB%8C%D9%85%D8%AA-%D8%AF%D9%84%D8%A7%D8%B1'
 
 
 response = requests.get(url)
-data = response.json()
+html_doc = response.text
 
-USD = data['rates']['USD']
 
-TOMAN = USD * 3794894.679351561
-print(f"{TOMAN} toman")
+soup = BeautifulSoup(html_doc, 'html.parser')
 
+td = soup.find('td').contents[0]
+td = td.split(",")
+td = int(td[0]+td[1])
+price = int(user)*td
+
+
+print(price)
 			
